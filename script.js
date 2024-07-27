cd3355b8d8b544f2bea4662891e8af7bcb91bb35
Shakhriyor <109933495+ShakhriyorIsobekov@users.noreply.github.com>
2024-07-27 21:24:19 +0500

    make changes in javascript

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const elModal = document.querySelector(".modal");
 const modalOpener = document.querySelector(".main__ad-btn");
 
-modalOpener.addEventListener("click", function() {
-   elModal.classList.add("modal__open");
-})
+if(modalOpener) {
+
+   modalOpener.addEventListener("click", function() {
+      elModal.classList.add("modal__open");
+   })
+}
 
 const modalCloser = document.querySelector(".main__modal-close-btn");
-
-modalCloser.addEventListener("click", function() {
-   elModal.classList.remove("modal__open");
-})
+if(modalCloser) {
+   modalCloser.addEventListener("click", function() {
+      elModal.classList.remove("modal__open");
+   })
+}
